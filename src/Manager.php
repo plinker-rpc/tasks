eff12d672d96ef55c9e425ee029a084e3e469245
Your Name <you@example.com>
2017-07-29 18:32:05 +0000

    .

--- src/Manager.php
+++ src/Manager.php
@@ -1,7 +1,6 @@
 <?php
 namespace Plinker\Tasks {
 
-    use RedBeanPHP\R;
     use Opis\Closure\SerializableClosure;
 
     class Manager {
@@ -15,19 +14,93 @@ namespace Plinker\Tasks {
             
             // load model
             $this->model = new Model($this->config['database']);
+            //$this->task = new Task($this->config['database']);
         }
-        
-        public function log(array $params = array())
-        {
-            return $this->model->findAll('log');
-        }
-        
+
         /**
          * 
          */
         public function create(array $params = array())
         {
-            return 'Task created';
+            if ($params[0] == 'task') {
+
+                // find or create new task source
+                $tasksource = $this->model->findOrCreate([
+                    'tasksource',
+                    'name' => $params[1][0]
+                ]);
+                        
+                // update - source
+                $tasksource->source = $params[1][1];
+                        
+                // update - created/updated date
+                if (empty($tasksource->created)) {
+                    $tasksource->created = date_create()->format('Y-m-d h:i:s'); 
+                } else {
+                    $tasksource->updated = date_create()->format('Y-m-d h:i:s'); 
+                }
+                        
+                // store
+                $this->model->store($tasksource);
+
+                //
+                return $this->model->export($tasksource)[0];
+            }
+            
+            return '$tasks->create(\''.$params[0].'\,...\') - Cant do that';
+        }
+        
+        /**
+         * 
+         */
+        public function get(array $params = array())
+        {
+            // get task 
+            return $this->model->findOne('tasksource', 'name = ?', [$params[0]]);
+        }
+
+        /**
+         * Run
+         * Puts task in tasking table for deamon to run.
+         */
+        public function run(array $params = array())
+        {
+            // find or create new task source
+            $task = $this->model->findOrCreate([
+                'tasks',
+                'name'   => $params[0],
+                'params' => json_encode($params[1]),
+                'completed' => 0
+            ]);
+
+            // get task source id
+            $task->tasksource = $this->model->findOne('tasksource', 'name = ?', [$params[0]]);
+            
+            // store task
+            $this->model->store($task);
+
+            return $this->model->export($task)[0];
+        }
+        
+        /**
+         * Run Now
+         * Does not put task in tasking table for deamon to run.
+         * note: will be run as apache user
+         */
+        public function runNow(array $params = array())
+        {
+            // get task 
+            $tasksource = $this->model->findOne('tasksource', 'name = ?', [$params[0]]);
+            
+            $source = unserialize($tasksource->source);
+            
+            $return = null;
+            ob_start();
+            $return = $source($params[1]);
+            $return = ob_get_contents().$return;
+            ob_end_clean();
+            
+            return $return;
         }
 
         /**
@@ -43,137 +116,20 @@ namespace Plinker\Tasks {
          */
         public function info(array $params = array())
         {
-            return 'Task info';
+            return $this->model->findOne('tasks', 'id = ?', [
+                $params[0]
+            ]);
         }
-
-        /**
-         * 
-         */
-        public function stop(array $params = array())
-        {
-            return 'Task stop';
-        }
-
+        
         /**
          * 
          */
         public function clear(array $params = array())
         {
-            return 'Task clear';
+            $this->model->exec('DELETE FROM tasks');
+            
+            return true;
         }
-
-        
-
-        // /**
-        //  * 
-        //  */
-        // public function announce(array $params = array())
-        // {
-        //     if (empty($params[0])) {
-        //         return new \Exception('Peer callback url is required.');
-        //     }
-            
-        //     $this->model->store($this->model->create(['log', 'time' => time(), 'action' => 'announce', 'ip' => $this->getIPAddress()]));
-            
-            
-        //     $peer = $this->model->findOrCreate([
-        //         'peer',
-        //         'ip' => $this->getIPAddress(),
-        //         'peer' => $params[0]
-        //     ]);
-
-        //     $peer->token = hash('sha256', (
-        //         $_SERVER['REMOTE_ADDR'].$params[0].microtime(true)
-        //     ));
-            
-        //     $this->model->store($peer);
-            
-        //     return $peer;
-        // }
-        
-        // public function peers(array $params = array())
-        // {
-        //     $this->model->store($this->model->create(['log', 'time' => time(), 'action' => 'peers', 'ip' => $this->getIPAddress()]));
-            
-        //     $peer = @$params[0];
-
-        //     if (!empty($peer['token'])) {
-        //         // find peer
-        //         $peer = $this->model->findOne('peer', 'token = ?', [
-        //             $peer['token']
-        //         ]);
-                
-        //         if (empty($peer)) {
-        //             return new \Exception('Unauthorized');
-        //         }
-                
-        //         return $this->model->findAll('peer');
-        //     } else {
-        //         return new \Exception('Peer announce record empty.');
-        //     }
-        // }
-        
-        // public function broadcast(array $params = array())
-        // {
-        //     $this->model->store($this->model->create(['log', 'time' => time(), 'action' => 'broadcast', 'ip' => $this->getIPAddress()]));
-            
-        //     $peer = @$params[0];
-
-        //     if (!empty($peer['token'])) {
-                
-        //         // find peer
-        //         $peer = $this->model->findOne('peer', 'token = ?', [
-        //             $peer['token']
-        //         ]);
-                
-        //         if (empty($peer)) {
-        //             return new \Exception('Unauthorized');
-        //         }
-                
-        //         $peers =  $this->model->findAll('peer');
-                
-        //         foreach ($peers as $id => $own_peer) {
-        //             $peer_network = new \Plinker\Core\Client(
-        //                 $own_peer->peer,
-        //                 'Peer\Peer',
-        //                 hash('sha256', gmdate('h').$this->config['plinker']['public_key']),
-        //                 hash('sha256', gmdate('h').$this->config['plinker']['private_key']),
-        //                 $this->config,
-        //                 $this->config['plinker']['encrypted']
-        //             );
-                    
-        //             $result[$id] = $peer_network->announce($peer['peer']);
-                    
-        //             $result[$id]['response'] = $peer_network->{@$params[1]}($result[$id]);
-        //         }
-
-        //     } else {
-        //         return new \Exception('Peer announce record empty.');
-        //     }
-
-        //     return $result;
-        // }
-
-        // public function disconnect(array $params = array())
-        // {
-        //     $peer =  $this->model->findOrCreate([
-        //         'peer',
-        //         'ip' => $this->getIPAddress()
-        //     ]);
-            
-        //     $this->model->trash($peer);
-            
-        //     return true;
-        // }
-        
-        // public function testClosure($params = array())
-        // {
-        //     $test = function ($what) {
-        //         return $what.' - Thanks buddy...';
-        //     };
-    
-        //     return new SerializableClosure($test);
-        // }
 
     }
 
